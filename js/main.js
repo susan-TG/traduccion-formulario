06413e8d29a5a3b76ac6c5d2f8f2f2c912c64712
susan-TG <susana.tralma.g@gmail.com>
2016-10-25 10:23:53 -0300

    termine de traducir los todos los textos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 function traducir() {
 	document.getElementById('form-signin-heading').innerHTML ="Por favor iniciar sesión"
 
+	var input = document.getElementById ("inputEmail");
+	input.placeholder = "por favor ingresa tu email"; 
+
+	var input = document.getElementById ("inputPassword");
+	input.placeholder = "contraseña";
+
+	document.querySelector ('.checkbox span').innerHTML ="recuerdame";
+
+	document.querySelector ('button').innerHTML ="inscribirme";
 }
